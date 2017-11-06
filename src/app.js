69657c643b15ffcbc09d0e667e1a7f6f3d3e2078
Mario <zhangbiaoFT@163.com>
2017-11-06 11:20:57 +0800

    gogo

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 const http = require('http');
-const chalk = require('chalk')
+const chalk = require('chalk');
+const path = require('path');
 const config = require('./config/defaultConfig');
+const route = require('./help/route')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    // res.write('');
-    res.end('Hello node!');
+    // const url = req.url;
+    const filePath = path.join(config.root, req.url);
+    route(req, res, filePath)
+    // fs.stat(filePath, (err, stats) => {
+    //     if (err) {
+            
+    //     }
+    //     if (stats.isFile()) {
+    //         res.statusCode = 200;
+    //         res.setHeader('Content-Type', 'text/plain');
+    //         fs.createReadStream(filePath).pipe(res);             
+    //     } else if(stats.isDirectory()) {
+    //         fs.readdir(filePath, (err, files) => {
+    //             res.statusCode = 200;
+    //             res.setHeader('Content-Type', 'text/plain');
+    //             res.end(files.join(','));
+    //         })
+    //     }
+    // })
+    // res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+    // // res.write('');
+    // res.end('Hello node!');
 });
 
 server.listen(config.port, config.hostname, () => {
